c190c4a326c990a4a54a091d78a0890dcf721dd9
Rifam29 <rifamazharulhaq.dhp@gmail.com>
2023-11-21 20:41:35 +0700

    commit

---
+++ maindo.js
@@ -0,0 +1,17 @@
+let sr = ScrollReveal ({
+    duration: 2500,
+    distance:"80px",
+});
+
+sr.reveal(".col-md-5", {origin:"top", delay: 200 });
+sr.reveal(".text-orange-name", {origin:"left", delay: 200 });
+sr.reveal(".section-title", {origin:"left", delay: 200 });
+sr.reveal(".header-title", {origin:"right", delay: 200 });
+sr.reveal(".text-secondary", {origin:"right", delay: 200 });
+sr.reveal(".header-skill", {origin:"bottom", delay: 200 });
+sr.reveal(".mb-3", {origin:"bottom", delay: 200 });
+sr.reveal(".col-md-4", {origin:"bottom", delay: 200 });
+sr.reveal(".text-orange-skills", {origin:"right", delay: 200 });
+sr.reveal(".text-orange-about", {origin:"bottom", delay: 200 });
+sr.reveal(".portfolio-section", {origin:"left", delay: 300 });
+sr.reveal(".section-title", {origin:"top", delay: 400 });
